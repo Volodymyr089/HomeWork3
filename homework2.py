a3cc838ce45af784304297a9fb89c038e715b8c7
Vova <shabanov089@gmail.com>
2022-01-26 14:21:11 +0200

    Initial commit

---
+++ homework2.py
@@ -0,0 +1,79 @@
+""""
+Дни недели:
+
+number = input('Введите номер дня: ')
+if number == '1':
+    print('Понедельник')
+elif number == '2':
+    print('Вторник')
+elif number == '3':
+    print('Среда')
+elif number == '4':
+    print('Четверг')
+elif number == '5':
+    print('Пятница')
+elif number == '6':
+    print('Суббота')
+elif number == '7':
+    print('Воскресенье')
+else:
+    print('В неделе всего 7 дней!')
+"""
+"""
+Месяца:
+
+number = input('Введите номер месяца: ')
+if number == '1':
+    print('Январь')
+elif number == '2':
+    print('Февраль')
+elif number == '3':
+    print('Март')
+elif number == '4':
+    print('Апрель')
+elif number == '5':
+    print('Май')
+elif number == '6':
+    print('Июнь')
+elif number == '7':
+    print('Июль')
+elif number == '8':
+    print('Август')
+elif number == '9':
+    print("Сентябрь")
+elif number == '10':
+    print('Октябрь')
+elif number == '11':
+    print('Ноябрь')
+elif number == '12':
+    print('Декабрь')
+else:
+    print('В году всего 12 месяцев!')
+"""
+
+"""
+Число ноль:
+
+x = 0
+n = int(input('Введите число: '))
+if x < n:
+    print('Number is positive')
+elif x > n:
+    print('Number is negative')
+elif x == n:
+    print('Number is equal to zero')
+else:
+    print('Error')
+"""
+
+
+# Это подсмотрел в инете(Не знал точно как решить порядок возрастания):
+"""
+x = int(input('Введите число 1: '))
+y = int(input('Введите число 2: '))
+if x == y:
+    print('Равные числа')
+else:
+    print(min(x,y))
+    print(max(x,y))
+"""
